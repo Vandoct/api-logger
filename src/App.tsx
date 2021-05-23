e68dbda8206ff7d2f113306abae1ef98ad56273c
Ichvandi Octa Maulana <ichvandiocta@gmail.com>
2021-05-23 21:55:30 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { FC, ReactElement } from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+const App: FC = (): ReactElement => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {process.env.REACT_APP_SECRET ||
+            'No ENV detected! | Create a copy from .env and rename to .env.local then restart server'}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
